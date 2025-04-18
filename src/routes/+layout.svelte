1418a9423b109794cc8cb39bcfd1925d062b02d3
Mika <mikacchang@gmail.com>
2025-04-18 17:28:23 -0400

    "updated pages on site"

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<script>
+    let { children } = $props();
+</script>
+
+<nav>
+    <a href="/">home</a>
+    <a href="/about">about</a>
+    <a href="/skills">skills</a>
+<!--    <a href="/projects">projects</a>-->
+</nav>
+
+{@render children()}
+
---
+++ src/routes/skills/+page.svelte
@@ -0,0 +1,21 @@
+{#snippet skill(description)}
+    <li>
+        {description}!
+    </li>
+{/snippet}
+
+<h1>Skills</h1>
+<h2>My Experience</h2>
+<ul>
+    <li>C# Application Design</li>
+</ul>
+
+<h2>Some skills I'd like to learn / am currently working on</h2>
+
+{@render skill("Web Development / UI")}
+{@render skill("Web Scrapping")}
+{@render skill("DB Design and Administration")}
+{@render skill("OAuth2 Authentication")}
+{@render skill("Networking")}
+{@render skill("Remote Procedure Calls")}
+{@render skill("/CD Pipelines")}
