d1d1465b272207a813c9ad0d8558e9750017d3df
Mika <mikacchang@gmail.com>
2025-04-18 23:24:01 -0400

    "updated navbar in layout"

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,11 +3,28 @@
 </script>
 
 <nav>
-    <a href="/">home</a>
-    <a href="/about">about</a>
-    <a href="/skills">skills</a>
+    <a href="/">HOME</a>
+    <a href="/about">ABOUT</a>
+    <a href="/skills">SKILLS</a>
 <!--    <a href="/projects">projects</a>-->
 </nav>
 
 {@render children()}
+
+<style>
+    nav {
+        background: #E0E0E0;
+        padding: 10px;
+        border-radius: 12px;
+        box-shadow: 0 4px 6px hsl(0, 0%, 70%), 0 5px 15px hsl(0, 0%, 70%);
+
+        a {
+            color: #000000;
+            font-weight: bold;
+            font-family: 'Verdana', sans-serif;
+            padding-left: 15px;
+            text-decoration: none;
+        }
+    }
+</style>
 
