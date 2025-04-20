9e1638ddfdfb787d09a76b626e360e48d61da530
Mika <mikacchang@gmail.com>
2025-04-19 20:58:39 -0400

    updated skills page

--- src/routes/skills/+page.svelte
+++ src/routes/skills/+page.svelte
@@ -1,7 +1,8 @@
 {#snippet skill(description)}
     <li>
-        {description}!
+        {description}
     </li>
+<!--    TODO: add associated project/experience info-->
 {/snippet}
 
 <h1>Skills</h1>
@@ -11,11 +12,12 @@
 </ul>
 
 <h2>Some skills I'd like to learn / am currently working on</h2>
-
-{@render skill("Web Development / UI")}
-{@render skill("Web Scrapping")}
-{@render skill("DB Design and Administration")}
-{@render skill("OAuth2 Authentication")}
-{@render skill("Networking")}
-{@render skill("Remote Procedure Calls")}
-{@render skill("/CD Pipelines")}
+<ul>
+    {@render skill("Web Development / UI")}
+    {@render skill("Web Scrapping")}
+    {@render skill("DB Design and Administration")}
+    {@render skill("OAuth2 Authentication")}
+    {@render skill("Networking")}
+    {@render skill("Remote Procedure Calls")}
+    {@render skill("CI/CD Pipelines")}
+</ul>
