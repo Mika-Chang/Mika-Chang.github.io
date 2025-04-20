e4d23567b99aa74b9a0bd87d5d81534a8963894b
Mika <mikacchang@gmail.com>
2025-04-19 22:30:47 -0400

    "Added a bio to the home page"

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,36 @@
-<h1>home</h1>
+<script>
+</script>
+
+<div class="container">
+    <div>
+        <img src="/images/cropped_pfp.jpeg" alt="A picture of me (Mika Chang)">
+    </div>
+    <div class="bio">
+        <h2>Mika Chang</h2>
+        <p>
+            Hi there! Thanks so much for checking out my site! I'm a developer that's into
+            automation, computer networking, and data ethics. In my free time I enjoy
+            working on personal projects, playing video games with my friends, running,
+            and spending time with my partner.
+        </p>
+    </div>
+</div>
+
+<style>
+    .container{
+        display: flex;
+        flex-wrap: wrap;
+        /*flex-direction: column;*/
+    }
+    img {
+        height: 300px;
+        width: 300px;
+        margin: 20px;
+        margin-top: 50px;
+        border-radius: 50%;
+        border-style: solid;
+        border-width: 8px;
+        border-color: #FF99A0;
+        box-shadow: 0 4px 10px hsl(0, 0%, 90%), 0 3px 10px hsl(0, 0%, 70%);
+    }
+</style>
