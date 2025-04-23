33c9222cc0a78a1b02db10c955bfcd530cb1bf3c
Mika <mikacchang@gmail.com>
2025-04-23 09:19:08 -0400

    "Removed contact page"

--- src/lib/Navbar.svelte
+++ src/lib/Navbar.svelte
@@ -5,7 +5,7 @@
 -->
 <script lang="ts">
     import {page} from '$app/state' // used for getting curr page
-    const pages: string[] = ["home", "skills", "projects", "resume", "contact"];
+    const pages: string[] = ["home", "skills", "projects", "resume"];
 
     /**
      * Get the route for a given page.
