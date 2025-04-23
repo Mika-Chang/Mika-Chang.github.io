ed7df6d919d788f78999299d1f12e7e0aeb3e584
Mika <mikacchang@gmail.com>
2025-04-23 12:00:14 -0400

    "Added resume"

--- src/routes/resume/+page.svelte
+++ src/routes/resume/+page.svelte
@@ -1,1 +1,16 @@
-<h1>Coming soon!</h1>
+<h1>My Resume</h1>
+<div>
+    <object class="pdf" data="/Mika_Chang_Resume.pdf" aria-label="Resume"></object>
+</div>
+
+<style>
+    div {
+        display: flex;
+        justify-content: center;
+    }
+    .pdf {
+        margin: 0 auto 1em auto;
+        width: 90%;
+        height: 90vw;
+    }
+</style>
