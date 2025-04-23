0b74f8be33587eab51e2eedf3e6003b45a0fc883
Mika <mikacchang@gmail.com>
2025-04-23 12:38:18 -0400

    "hid contact form for now"

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -2,8 +2,9 @@
     import ContactForm from "$lib/ContactForm.svelte";
     import Linkbar from "$lib/Linkbar.svelte";
 </script>
+
 <div class="footer">
-    <div style="grid-area: 1 1"><ContactForm /></div>
+<!--    <div style="grid-area: 1 1"><ContactForm /></div>-->
     <div style="grid-area: 2 1"><Linkbar /></div>
 </div>
 
