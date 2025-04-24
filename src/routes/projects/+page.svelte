5bf3cfa4aa087344fe8d5403ec554864b8e55203
Mika <mikacchang@gmail.com>
2025-04-24 10:24:12 -0400

    Added list of projects

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -1,1 +1,20 @@
-<h1>Coming soon!</h1>
+<div class="content">
+    <h1>My Projects</h1>
+    <h2>Personal</h2>
+    <h3>This Site</h3>
+    <h3>Jobscrapers</h3>
+    <h3>Home NAS / Media Server</h3>
+    <h2>TEL Manufacturing and Engineering of America</h2>
+    <h3>Code Migration Assistant Application</h3>
+    <h3>Data Transfer Automation</h3>
+    <h3>Software Deployment Automation</h3>
+    <h3>VDI Setup</h3>
+    <h2>UW &#8212 Madison Venturelli Lab</h2>
+    <h3>Bacterial Community Simulations</h3>
+</div>
+
+<style>
+    .content{
+        margin: 0 auto;
+    }
+</style>
