e47e4ff9bee149362484aa748e5db1954c477ac1
Mika <mikacchang@gmail.com>
2025-05-07 09:24:08 -0400

    Added resume download button

--- src/routes/resume/+page.svelte
+++ src/routes/resume/+page.svelte
@@ -1,9 +1,18 @@
 <h1>My Resume</h1>
+<a
+        aria-label="Resume Download Button"
+        href="/Mika_Chang_Resume.pdf"
+        download="Mika_Chang_Resume">
+    <button>Download</button>
+</a>
 <div>
     <object class="pdf" data="/Mika_Chang_Resume.pdf" aria-label="Resume"></object>
 </div>
 
 <style>
+    a {
+        margin: 0px auto 20px auto;
+    }
     h1 {
         text-align: center;
     }
@@ -15,5 +24,13 @@
         margin: 0 auto 1em auto;
         width: 90%;
         height: 90vw;
+    }
+    button {
+        padding: 10px 20px;
+        background-color: var(--bg-2);
+        color: white;
+        font-weight: bold;
+        border-radius: 10px;
+        text-decoration: none;
     }
 </style>
