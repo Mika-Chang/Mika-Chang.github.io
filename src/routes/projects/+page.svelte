9df280ea1b61630fd4c469c23cf5e97c9e4ca5f8
Mika <mikacchang@gmail.com>
2025-06-21 12:09:21 -0400

    Updated projects page

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -1,20 +1,33 @@
 <div class="content">
     <h1>My Projects</h1>
-    <h2>Personal</h2>
-    <h3>This Site</h3>
-    <h3>Jobscrapers</h3>
-    <h3>Home NAS / Media Server</h3>
     <h2>TEL Manufacturing and Engineering of America</h2>
     <h3>Code Migration Assistant Application</h3>
+    <p>
+        In my role as a Software Engineering Intern at TEL, I was responsible for developing tools used for a code migration project. Utilizing Agile methodologies and the .NET application platform, I developed user-friendly software that could analyze old code to visualize it in graphics such as flow charts, rewrite it in different formats, and provide information such as where variables were used in other files in the project. Furthermore, I implemented an installer and extensive documentation allowing developers from other teams to quickly start using the tool. As a result, developers new to the project could quickly start understanding code, and the logic of parts of the code could be more easily explained to people without a technical background.
+    </p>
     <h3>Data Transfer Automation</h3>
+    <p>
+        I have experience streamlining ETL pipelines for data analysis. At TEL, I was  responsible for giving our data analysis team real-time access to data from simulated tools. Leveraging transactional replication from a SQL Server installed on our tools, I implemented a process to stream tool data to our data analysis team. Database replication removed the bottleneck of manual data transfer and freed up valuable tool time. The ability to access more data has enabled us to further our digital transformation efforts. Thanks to the SQL replication and collaboration with the data analysis team, TEL started designing workflows that will enable fine-grained analytics to detect tool issues earlier and save thousands of dollars per semiconductor wafer.
+    </p>
     <h3>Software Deployment Automation</h3>
+    <p>
+        In my role as a Software Engineering Intern at TEL, I migrated software deployment for one of our from step-by-step instructions to an automated process using an installer. Our team wanted to deploy our software quickly and easily while accounting for the fact that different tools would need different patch versions, and it was my responsibility to implement the installer. As a result, software installation that may have taken a day or more now takes a few minutes. In addition to creating a flexible installer, I ensured that it was well documented and designed in a way that would be easy for other team members to update.
+    </p>
     <h3>VDI Setup</h3>
+    <p>
+        In my role as a Software Engineering Intern at TEL, one of my responsibilities was setting up virtual desktop infrastructure (VDI) for my team, saving them time when testing software. I collaborated with our IT team through meetings and help desk tickets to go over specifications for the necessary devices. As a result, I gained access to and configured 5 Windows environments for our team. This allowed us to simulate our products and perform many tests at once. Additionally, access to the VDIs enabled new members to use software that could take weeks to acquire on their own devices. I also documented my helpdesk tickets and set up standardized processes for the team, so I could support my team even after working at TEL.
+    </p>
     <h2>UW &#8212 Madison Venturelli Lab</h2>
     <h3>Bacterial Community Simulations</h3>
+    <h2>Personal Projects</h2>
+    <h3>Home NAS / Media Server</h3>
+    <h3>Jobscrapers</h3>
+    Project to scrape job listing from Workday sites and output them as json data.
+    <h3>This Site</h3>
 </div>
 
-<style>
-    .content{
-        margin: 0 auto;
-    }
-</style>
+<!--<style>-->
+<!--    .content{-->
+<!--        margin: auto auto;-->
+<!--    }-->
+<!--</style>-->
