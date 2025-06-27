8434718c3e44f24320b6cf4cc091d517474ac95d
Mika <mikacchang@gmail.com>
2025-06-26 21:39:07 -0400

    implemented conditional adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,16 +1,21 @@
-//import adapter from '@sveltejs/adapter-static';
-import adapter from '@sveltejs/adapter-node';
+// conditional sk adapter from https://www.ryanfiller.com/blog/tips/conditional-sveltekit-adapters
+import dotenv from 'dotenv'
+dotenv.config()
+
+import adapterNode from '@sveltejs/adapter-node';
+import adapterStatic from '@sveltejs/adapter-static';
 
 export default {
 	kit: {
-		adapter: adapter({
+		adapter: process.env.ADAPTER === 'static'
+			? adapterStatic({
 			// default options are shown. On some platforms
 			// these options are set automatically — see below
 			pages: 'build',
 			assets: 'build',
 			fallback: '404.html',
 			precompress: false,
-			strict: true
-		})
+			strict: true})
+			: adapterNode({})
 	}
 };
